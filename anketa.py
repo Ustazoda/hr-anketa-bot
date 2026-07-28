bf54e8b089d71d72e8428f4e65907a38a0a9eee9
Ustazoda <sohibjonustaboyev@gmail.com>
2026-07-28 15:48:59 +0500

    Update anketa.py

--- anketa.py
+++ anketa.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 
 # === SOZLAMALAR (Environment Variables orqali olinadi) ===
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7634467401:AAGBpV1MoC0qzeo1_8OS0bXcc6NZ3_uQubI")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "1168952611"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "766309793"))
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "YOUR_GEMINI_API_KEY")
 
 # Yangi Gemini Client
